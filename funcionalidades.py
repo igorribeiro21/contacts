a68a49f4b52816a46dccec598cdb30296ac47fab
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-06 08:23:50 -0300

    main - refactor: contact schedule project completed

--- funcionalidades.py
+++ funcionalidades.py
@@ -19,3 +19,64 @@ def func_adicionar_contato():
 def func_visualizar_contatos():
     visualizar_contatos()
     return
+
+def func_editar_contato():
+    visualizar_contatos()
+    indice = int(input("\n Digite o contato que deseja atualizar: "))
+        
+    contato = buscar_contato_indice(indice)
+
+    nome = contato["nome"]
+    email = contato["email"]
+    telefone = contato["telefone"]
+    favorito = "Sim" if contato["favorito"] else "Não"
+    nome_atualizado = input(f"Digite o nome para ser atualizado atualmente está '{nome}':")
+    email_atualizado = input(f"Digite o e-mail para ser atualizado atualmente está '{email}':")
+    telefone_atualizado = input(f"Digite o telefone para ser atualizado atualmente está '{formatar_numero_telefone(telefone)}', digite no formato DDNumero juntos por ex: 11999998888:")
+    favorito_atualizado = input(f"Este contato atualmente {favorito} é favorito, ele deve ficar como Sim ou Não? (S/N):")
+
+    favorito = False
+
+    if(favorito_atualizado == "S"):
+        favorito = True
+    else:
+        favorito = False
+
+    contato = { "nome": nome_atualizado, "telefone": telefone_atualizado, "email": email_atualizado, "favorito": favorito }
+    editar_contato(indice,contato)
+    return
+
+def func_marcar_desmarcar_favorito():
+    status = False
+    visualizar_contatos()
+    indice = input("\n Digite o número do contato que deseja alterar o status de favorito: ")
+
+    contato = buscar_contato_indice(indice)
+    mensagem = ""
+
+    if(contato["favorito"]):
+        mensagem = "Deseja alterar esse contato para não favorito? (S/N)"
+    else:
+        mensagem = "Deseja alterar esse contato para favorito? (S/N)"
+
+    resposta_marcar_desmarcar = input(mensagem)
+
+    marcar_desmarcar = True if resposta_marcar_desmarcar == "S" else False
+
+    if(marcar_desmarcar):
+        status =  False if contato["favorito"] else True
+    else:
+        status = contato["favorito"]
+        
+    marcar_desmarcar_favorito(indice, status)
+    return
+
+def func_buscar_contatos_favoritos():
+    buscar_contatos_favoritos()
+    return
+
+def func_apagar_contato():
+    visualizar_contatos()
+    indice = input("\n Digite o número do contato que deseja apagar: ")
+    apagar_contato(indice)
+    return
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from funcoes import adicionar_contato,visualizar_contatos,editar_contato,buscar_contato_indice,marcar_desmarcar_favorito,buscar_contatos_favoritos,apagar_contato, formatar_numero_telefone
-from funcionalidades import func_adicionar_contato,func_visualizar_contatos
+from funcionalidades import func_adicionar_contato,func_visualizar_contatos,func_editar_contato,func_marcar_desmarcar_favorito,func_buscar_contatos_favoritos,func_apagar_contato
 
 while True:
     print("\n Menu da lista de opções:")
@@ -18,58 +17,13 @@ while True:
     elif escolha == 2:
         func_visualizar_contatos()
     elif escolha == 3:
-        visualizar_contatos()
-        indice = int(input("\n Digite o contato que deseja atualizar: "))
-        
-        contato = buscar_contato_indice(indice)
-
-        nome = contato["nome"]
-        email = contato["email"]
-        telefone = contato["telefone"]
-        favorito = "Sim" if contato["favorito"] else "Não"
-        nome_atualizado = input(f"Digite o nome para ser atualizado atualmente está '{nome}':")
-        email_atualizado = input(f"Digite o e-mail para ser atualizado atualmente está '{email}':")
-        telefone_atualizado = input(f"Digite o telefone para ser atualizado atualmente está '{formatar_numero_telefone(telefone)}', digite no formato DDNumero juntos por ex: 11999998888:")
-        favorito_atualizado = input(f"Este contato atualmente {favorito} é favorito, ele deve ficar como Sim ou Não? (S/N):")
-
-        favorito = False
-
-        if(favorito_atualizado == "S"):
-            favorito = True
-        else:
-            favorito = False
-
-        contato = { "nome": nome_atualizado, "telefone": telefone_atualizado, "email": email_atualizado, "favorito": favorito }
-        editar_contato(indice,contato)
+        func_editar_contato()
     elif escolha == 4:
-        status = False
-        visualizar_contatos()
-        indice = input("\n Digite o número do contato que deseja alterar o status de favorito: ")
-
-        contato = buscar_contato_indice(indice)
-        mensagem = ""
-
-        if(contato["favorito"]):
-            mensagem = "Deseja alterar esse contato para não favorito? (S/N)"
-        else:
-            mensagem = "Deseja alterar esse contato para favorito? (S/N)"
-
-        resposta_marcar_desmarcar = input(mensagem)
-
-        marcar_desmarcar = True if resposta_marcar_desmarcar == "S" else False
-
-        if(marcar_desmarcar):
-            status =  False if contato["favorito"] else True
-        else:
-            status = contato["favorito"]
-        
-        marcar_desmarcar_favorito(indice, status)
+        func_marcar_desmarcar_favorito()
     elif escolha == 5:
-        buscar_contatos_favoritos()
+        func_buscar_contatos_favoritos()
     elif escolha == 6:
-        visualizar_contatos()
-        indice = input("\n Digite o número do contato que deseja apagar: ")
-        apagar_contato(indice)
+        func_apagar_contato()
     elif escolha == 7:
         break
 
